ab2dd5688360125d56ed1ebe26777a6336fe3fd6
kelly <kelly.n.waite@gmail.com>
2016-11-26 15:37:59 -0700

    Build DoGood form

--- src/app/components/DoGoodForm.js
+++ src/app/components/DoGoodForm.js
@@ -5,9 +5,35 @@ import React from 'react';
 export default class DoGoodForm extends React.Component {
   render () {
     return (
-      <div>
-        <p>Put a form in here</p>
-      </div>
+      <form>
+        <div>
+          <label for="name">Name:
+            <input type="text" id="name" />
+          </label>
+        </div>
+        <div>
+          <label for="amount">Amount Donated:
+            <input type="text" id="amount" />
+          </label>
+        </div>
+        <div>
+          <label for="donateTo">Donated to:
+            <input type="text" id="donatedTo" />
+          </label>
+        </div>
+        <div>
+          <label for="gaveUp">What did you give up to donate?
+            <textarea type="text" id="gaveUp" />
+          </label>
+        </div>
+        <div>
+          <label for="socialMedia">Share on:
+            <input type="checkbox" id="facebook" /> Facebook <br />
+            <input type="checkbox" id="twitter" /> Twitter
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     )
   }
 }
