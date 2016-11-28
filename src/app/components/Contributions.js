02406c57652fd4cd6f6b7bd2b654eb5e8e66bd6a
kelly <kelly.n.waite@gmail.com>
2016-11-28 15:40:11 -0700

    Add total of donations; Delete ContTotal

--- src/app/components/Contributions.js
+++ src/app/components/Contributions.js
@@ -30,16 +30,17 @@ export default class Contributions extends React.Component {
 }
 
   render () {
+    let total = 0;
+    this.state.doGood.map((c,i,a) => {
+      total += c.amount
+      return total
+    })
     return (
       <div>
         <h2>Collective Contributions</h2>
         <p>Small acts can make a big difference. Just look at all the good being done by people donating their hidden surplus. Way to make the world a better place!</p>
         <h3>Total</h3>
-        {this.state.doGood.map((c,i,a) => {
-          return (
-            <ContTotals doGood={c.amount} />
-          )
-        })}
+        <h1>{total}</h1>
 
       </div>
     )
