3782a9933d9cb4cefabf979772bc32a4c8270818
kelly <kelly.n.waite@gmail.com>
2016-11-28 14:58:13 -0700

    Pull data from firebase in contributions and pass to ContTotals

--- src/app/components/ContTotals.js
+++ src/app/components/ContTotals.js
@@ -3,11 +3,17 @@
 import React from 'react';
 import { fbRef, fbDoGood } from './constants';
 
+
 export default class ContTotals extends React.Component {
+
   render () {
+    console.log(this.props.doGood);
     return (
       <div>
         <h3>Total Amount of Contributions</h3>
+        <h1>
+        Hello
+        </h1>
       </div>
     )
   }
--- src/app/components/Contributions.js
+++ src/app/components/Contributions.js
@@ -4,13 +4,38 @@ import React from 'react';
 
 import ContTotals from './ContTotals';
 
+import { fbRef, fbDoGood } from './constants';
+
+let doGood = [];
+
+function updateToDo (val, id) {
+  doGood.push(val);
+}
+
 export default class Contributions extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = {
+    doGood: doGood,
+  }
+};
+
+  componentDidMount() {
+  fbRef.on("child_added", (snapshot) => {
+    updateToDo(snapshot.val(), snapshot.key);
+    this.setState({
+      doGood: doGood,
+    });
+  }).bind(this)
+}
+
   render () {
+    console.log("test",this.state.doGood);
     return (
       <div>
         <h2>Collective Contributions</h2>
         <p>Small acts can make a big difference. Just look at all the good being done by people donating their hidden surplus. Way to make the world a better place!</p>
-        <ContTotals />
+        <ContTotals doGood={this.state.doGood} />
       </div>
     )
   }
